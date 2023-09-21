b39f8bdd4eba37ee15e9abf2dd8e50435287d70f
tharhtetzan1 <tharhtetzan13@gmail.com>
2023-09-22 03:03:08 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["**.html",],
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          100: "#d3e0fb",
+          200: "#a8c1f7",
+          300: "#7ca1f3",
+          400: "#5182ef",
+          500: "#2563eb",
+          600: "#1e4fbc",
+          700: "#163b8d",
+          800: "#0f285e",
+          900: "#07142f"
+        },
+        
+      }
+
+
+    },
+  },
+  plugins: [],
+}
+
